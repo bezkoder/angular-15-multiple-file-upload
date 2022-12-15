35f8521a502684a6f09ae92ddc83b796ce0ecb96
tienbku <tien.ngtruong@gmail.com>
2022-12-15 11:41:59 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-15-multiple-file-upload';
+  title = 'Angular 15 Multiple Files Upload';
 }
